6337076784c5b6ac43e64bc655b5256bb046ae32
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-14 22:22:01 -0300

    Creating the interface repository of user and transaction

---
+++ API.Pagamentos/API.Pagamentos/Repositories/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.Pagamentos.Domain;
+
+namespace API.Pagamentos.Repositories.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task<IEnumerable<Transaction>> GetTransactionsAsync();
+        Task<Transaction> GetByIdAsync(int? id);
+        Task<Transaction> CreateAsync(Transaction transaction);
+    }
+}
---
+++ API.Pagamentos/API.Pagamentos/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.Pagamentos.Domain;
+
+namespace API.Pagamentos.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User> GetByIdAsync(int? id);
+        Task<User> CreateAsync(User user);
+    }
+}
