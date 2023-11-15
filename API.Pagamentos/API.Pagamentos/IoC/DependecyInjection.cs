230d9ebcdadbafa108f4dd78905460307c272894
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-14 22:00:58 -0300

    Adding the dependecy injection container

---
+++ API.Pagamentos/API.Pagamentos/IoC/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using API.Pagamentos.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Pagamentos.IoC
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
--- API.Pagamentos/API.Pagamentos/Program.cs
+++ API.Pagamentos/API.Pagamentos/Program.cs
@@ -1,8 +1,12 @@
+using API.Pagamentos.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
