5afdf5533d572226b9b20124cf09a196712b78ef
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-15 14:02:13 -0300

    Creating the repositories for transaction and user

---
+++ API.Pagamentos/API.Pagamentos/Repositories/TransactionRepository.cs
@@ -0,0 +1,34 @@
+﻿using API.Pagamentos.Context;
+using API.Pagamentos.Domain;
+using API.Pagamentos.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Pagamentos.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TransactionRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactionsAsync()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        public async Task<Transaction> GetByIdAsync(int? id)
+        {
+            return await _context.Transactions.FindAsync(id) ?? throw new ArgumentNullException();
+        }
+
+        public async Task<Transaction> CreateAsync(Transaction transaction)
+        {
+            _context.Add(transaction);
+            await _context.SaveChangesAsync();
+            return transaction;
+        }
+    }
+}
---
+++ API.Pagamentos/API.Pagamentos/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using API.Pagamentos.Context;
+using API.Pagamentos.Domain;
+using API.Pagamentos.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Pagamentos.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int? id)
+        {
+            return await _context.Users.FindAsync(id) ?? throw new ArgumentNullException();
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
