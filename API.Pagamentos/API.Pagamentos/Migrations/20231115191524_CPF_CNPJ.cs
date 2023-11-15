7b0e2a9c5250db6bf81cb9faee83941edd8d8b1b
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-15 16:17:11 -0300

    Adding migration  CPF_CNPJ

---
+++ API.Pagamentos/API.Pagamentos/Migrations/20231115191524_CPF_CNPJ.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Pagamentos.Migrations
+{
+    public partial class CPF_CNPJ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CNPJ",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CPF_CNPJ",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF_CNPJ",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CNPJ",
+                table: "Users",
+                type: "nvarchar(18)",
+                maxLength: 18,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Users",
+                type: "nvarchar(18)",
+                maxLength: 18,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
